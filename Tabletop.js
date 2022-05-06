cfd0c21fa34dd04a854d201c4592276cea981f0a
SafwanBackar <sbackar29@gmail.com>
2022-05-06 13:13:06 +0530

    submit form added

---
+++ Tabletop.js
@@ -0,0 +1,10 @@
+// function init() {
+//     Tabletop.init({
+//         key: 'https://docs.google.com/spreadsheets/d/13A6-__kI4SyJLvd-bNfaNOezzmNm2trV0jp6v3C0qhk/edit#gid=0',
+//         callback: function (data, tabletop) {
+//             console.log(data)
+//         },
+//         simpleSheet: true
+//     })
+// }
+// window.addEventListener('DOMContentLoaded', init)
